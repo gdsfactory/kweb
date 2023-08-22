3ccf73b8804017057b0df79fa0af8f535ce99e67
Sebastian Goeldi <sebastian.goeldi@protonmail.com>
2023-08-22 22:07:47 +0200

    switch to pydantic.v1 in case of pydantic2 to avoid depending on pydantic-settingsw

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# kweb 1.1.1
+# kweb 1.1.2
 
 KLayout Web Viewer ![demo](docs/_static/kweb.png)
 
--- src/kweb/config.py
+++ src/kweb/config.py
@@ -5,28 +5,34 @@ from pathlib import Path
 import pydantic
 
 if int(pydantic.__version__.split(".")[0]) >= 2:
-    from pydantic_settings import BaseSettings, SettingsConfigDict
+    import pydantic.v1 as pydantic  # type: ignore[no-redef]
 
-    class Config(BaseSettings):
-        model_config = SettingsConfigDict(env_prefix="kweb_", env_nested_delimiter="_")
-        fileslocation: Path
+# if int(pydantic.__version__.split(".")[0]) >= 2:
+#     from pydantic_settings import BaseSettings, SettingsConfigDict
 
-        @pydantic.field_validator("fileslocation")
-        @classmethod
-        def resolvefileslocation(cls, v: Path | str) -> Path:
-            return Path(v).expanduser().resolve()
+#     class Config(BaseSettings):
+#        model_config = SettingsConfigDict(
+#            env_prefix="kweb_", env_nested_delimiter="_"
+#        )
+#         fileslocation: Path
 
-else:
-    from pydantic import BaseSettings  # type: ignore[no-redef]
+#         @pydantic.field_validator("fileslocation")
+#         @classmethod
+#         def resolvefileslocation(cls, v: Path | str) -> Path:
+#             return Path(v).expanduser().resolve()
 
-    class Config(BaseSettings):  # type: ignore[no-redef]
-        class Config:
-            env_prefix = "kweb_"
-            env_nested_delimiter = "_"
+# else:
+#     from pydantic import BaseSettings  # type: ignore[no-redef]
 
-        fileslocation: Path
 
-        @pydantic.validator("fileslocation")
-        @classmethod
-        def resolvefileslocation(cls, v: Path | str) -> Path:
-            return Path(v).expanduser().resolve()
+class Config(pydantic.BaseSettings):  # type: ignore[valid-type, misc]
+    class Config:
+        env_prefix = "kweb_"
+        env_nested_delimiter = "_"
+
+    fileslocation: Path
+
+    @pydantic.validator("fileslocation")
+    @classmethod
+    def resolvefileslocation(cls, v: Path | str) -> Path:
+        return Path(v).expanduser().resolve()
